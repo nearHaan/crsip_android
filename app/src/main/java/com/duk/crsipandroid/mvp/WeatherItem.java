4f1e5f08e987a1a25e1ee1c34346c443c16c9bb2
nearHaan <farhaan.nizam675@gmail.com>
2025-06-18 09:52:40 +0530

    Added wind element

--- app/src/main/java/com/duk/crsipandroid/mvp/WeatherItem.java
+++ app/src/main/java/com/duk/crsipandroid/mvp/WeatherItem.java
@@ -9,13 +9,16 @@ public class WeatherItem {
     private String dtTxt;
     private MainWeather main;
     private List<Weather> weather;
+    private WindWeather wind;
 
-    public WeatherItem(String dtTxt, MainWeather main, List<Weather> weather){
+    public WeatherItem(String dtTxt, MainWeather main, List<Weather> weather, WindWeather wind){
         this.dtTxt = dtTxt;
         this.main = main;
         this.weather = weather;
+        this.wind = wind;
     }
     public String getDtTxt() { return dtTxt; }
     public MainWeather getMain() { return main; }
     public List<Weather> getWeather() { return weather; }
+    public WindWeather getWind() { return wind; }
 }
---
+++ app/src/main/java/com/duk/crsipandroid/mvp/WindWeather.java
@@ -0,0 +1,11 @@
+package com.duk.crsipandroid.mvp;
+
+public class WindWeather {
+    private double speed;
+
+    public WindWeather(double speed){
+        this.speed = speed;
+    }
+
+    public double getSpeed(){ return speed; }
+}
