43eb5df627f2fbfd9d87a1fbb71dfb78bc9b2f86
nearHaan <farhaan.nizam675@gmail.com>
2025-06-19 11:58:01 +0530

    Adaptive TextureView

---
+++ app/src/main/java/com/duk/crsipandroid/utils/AutoFitTextureView.java
@@ -0,0 +1,46 @@
+// AutoFitTextureView.java
+package com.duk.crsipandroid.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+public class AutoFitTextureView extends TextureView {
+    private int ratioWidth = 0;
+    private int ratioHeight = 0;
+
+    public AutoFitTextureView(Context context) {
+        super(context);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setAspectRatio(int width, int height) {
+        if (width < 0 || height < 0) throw new IllegalArgumentException("Size cannot be negative.");
+        ratioWidth = width;
+        ratioHeight = height;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (0 == ratioWidth || 0 == ratioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * ratioWidth / ratioHeight) {
+                setMeasuredDimension(width, width * ratioHeight / ratioWidth);
+            } else {
+                setMeasuredDimension(height * ratioWidth / ratioHeight, height);
+            }
+        }
+    }
+}
